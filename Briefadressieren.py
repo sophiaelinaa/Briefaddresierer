d0f9ca769f45c746d79d956fd58e0b6b2a2a1413
sophiaelinaa <96128600+sophiaelinaa@users.noreply.github.com>
2022-01-11 15:32:34 +0100

    Add files via upload

---
+++ Briefadressieren.py
@@ -0,0 +1,14 @@
+def adresse(Vorname, Name, Strasse, Strassennr, Plz, Ort):
+    #Hier habe ich hingeschrieben was die Variable "adresse" alles enthalten muss
+    print (Vorname+"  "+Name)
+    #"  "ist für Abstand. In der ersten Linie soll es Vorname und Name printen
+    print (Strasse+"  "+Strassennr)
+    # print eine Zeile weiter runtergehen um nächsten Text eine Zeile weiter unten, in dieser Zeile soll es die Strasse und Strassennummer schreiben
+    print (Plz+"  "+Ort)
+    #In dieser Zeile soll der Output die Plz und der Ort sein
+    print("============================")
+
+adresse ("Max", "Mustermann","Musterstrasse","1","6000","Musterstadt")
+adresse ("Paul","Mustermann","Musterstrasse","1","6000","Musterstadt")
+#in diesen zwei Zeilen sind die zwei Adressen die nach dem obrigen Prinzip geprintet werden sollen
+
